c0293ed15ae50578668278864adaf0c72f6d1113
Keunbae Park <ppotpo@gmail.com>
2021-12-19 02:47:42 -0500

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,13 @@
 <?php
 
+$finder = Symfony\Component\Finder\Finder::create()
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php');
+
 return (new PhpCsFixer\Config())
     ->setUsingCache(false)
     ->setRiskyAllowed(true)
@@ -14,4 +22,5 @@ return (new PhpCsFixer\Config())
         'phpdoc_separation' => false,
         'no_superfluous_phpdoc_tags' => false,
         'php_unit_method_casing' => ['case' => 'snake_case'],
-    ]);
+    ])
+    ->setFinder($finder);
